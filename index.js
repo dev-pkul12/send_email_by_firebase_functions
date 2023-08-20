bcf4d98043f91051218e977aaddbeea374707e6f
Prasanna Kulkarni <104126691+prasannakulkarni12@users.noreply.github.com>
2023-08-20 21:00:22 +0530

    send email feature

--- index.js
+++ index.js
@@ -7,26 +7,38 @@ admin.initializeApp();
 // Set your SendGrid API key here
 sgMail.setApiKey("YOUR_SENDGRID_API_KEY");
 
+// Function to send an email using SendGrid
 const sendEmail = (eventType, data) => {
-    const msg = {
-        to: "recipient@example.com",
-        from: "sender@example.com",
-        subject: `Firestore Document ${eventType}`,
-        text: `Document ID: ${data.id} \nData: ${JSON.stringify(data)}`,
-    };
+  const msg = {
+    to: "recipient@example.com", // Replace with recipient's email address
+    from: "sender@example.com",   // Replace with sender's email address
+    subject: `Firestore Document ${eventType}`,
+    text: `Document ID: ${data.id} \nData: ${JSON.stringify(data)}`,
+  };
 
-    sgMail.send(msg).then(() => {
-        console.log(`Email sent for ${eventType} event`);
-    }).catch((error) => {
-        console.error(`Error sending email: ${error}`);
+  // Send the email using SendGrid
+  sgMail.send(msg)
+    .then(() => {
+      console.log(`Email sent for ${eventType} event`);
+    })
+    .catch((error) => {
+      console.error(`Error sending email: ${error}`);
     });
 };
 
+// Cloud Function triggered when a document is created, updated, or deleted
 exports.firestoreListener = functions.firestore
-    .document("yourCollection/{documentId}")
-    .onWrite((change, context) => {
-        const eventType = change.after.exists ? "updated" : "deleted";
-        const data = change.after.exists ? change.after.data() : change.before.data();
-        sendEmail(eventType, { id: context.params.documentId, data });
-        return null;
-    });
+  .document("yourCollection/{documentId}") // Replace with your Firestore collection path
+  .onWrite((change, context) => {
+    // Determine the event type (created, updated, or deleted)
+    const eventType = change.after.exists ? "updated" : "deleted";
+    
+    // Get the document data based on the event type
+    const data = change.after.exists ? change.after.data() : change.before.data();
+    
+    // Call the sendEmail function to send an email
+    sendEmail(eventType, { id: context.params.documentId, data });
+
+    // The function doesn't return any value, so we return null
+    return null;
+  });
